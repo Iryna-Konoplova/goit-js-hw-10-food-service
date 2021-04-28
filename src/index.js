dc4d5b6c4985abbd2386561ce038df62f0ad30c9
Iryna Konoplova <a.irina773@gmail.com>
2021-04-29 01:39:25 +0300

    Добавляет стартовую разметку и стили

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const body = document.querySelector('body');
+const themeSwitch = document.querySelector('.theme-switch__toggle');
+themeSwitch.addEventListener('change', onToggleSwitchChange)
+
+function onToggleSwitchChange(e) {
+    if (!body.classList.contains('dark-theme')) {
+        body.classList.add('dark-theme');
+        localStorage.setItem( 'Theme', 'dark-theme');
+        
+    } else {
+       body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        localStorage.setItem( 'Theme', 'light-theme');
+    }
+   
+  
+}
